298009289c6d2325051709160acaad1d91c2046b
Lara H. ABY <145819936+larahisham@users.noreply.github.com>
2024-12-28 19:37:56 +0300

    Update hs.cs

--- hs.cs
+++ hs.cs
@@ -1,40 +1,41 @@
 using System;
 using System.IO;
 using System.Text.Json;
-using static IOFiles;
-interface User
-{
-	public void Login(string ID, string Password);
-	public void Logout();
-	public void NewUser(string ID, string Name, string Password);
-}
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Diagnostics;
 [Serializable]
-public class Guest : User 
+public class Guest 
 {
 	public Guest(string ID, string Name, string Password, double PhoneNumber, double Bankbalance)
 	{
-		this.id = ID;
-		this.name = Name;	
-		this.password = Password;
-		this.bankbalance = Bankbalance;
-		this.phonenumber = PhoneNumber;
+			this.id = ID;
+	        this.name = Name; 
+			this.password = Password;
+			this.bankbalance = Bankbalance;
+			this.phonenumber = PhoneNumber;
 	}
-	private string id;
-	public string ID 
+	public Guest(string ID, string Password)
 	{
-		get 
+		this.id = ID;
+		this.password = Password;
+	}
+
+	private string id;
+	public string ID
+	{
+		get
 		{
 			return this.id;
 		}
-		set 
+		set
 		{
 
-		} 
-	} 
+		}
+	}
 	private string name;
-	public string Name 
+	public string Name
 	{
-		get 
+		get
 		{
 			return this.name;
 		}
@@ -42,7 +43,7 @@ public class Guest : User
 		{
 
 		}
-	} 
+	}
 	private string password;
 	public string Password
 	{
@@ -79,12 +80,14 @@ public class Guest : User
 
 		}
 	}
-	public void Login(string ID, string Password) { } 
-	public void Logout() { } 
-	public void NewUser(string ID, string Name, string Password) { }
+	// when new user is registered, this is appear 
+	public override string ToString()
+	{
+		return "Your name is : " + name + "\nYour ID is : " + id + "\nYour Phone Number is : " + phonenumber + "\nYour Bank Balance is : " + bankbalance;
+	}
 }
 [Serializable]
-public class Manager : User 
+public class Manager 
 {
 	private string id;
 	public string ID
@@ -92,10 +95,6 @@ public class Manager : User
 		get
 		{
 			return this.id;
-		}
-		set
-		{
-
 		}
 	}
 	private string password;
@@ -105,38 +104,40 @@ public class Manager : User
 		{
 			return this.password;
 		}
-		set
-		{
-
-		}
 	}
+	private bool IsManagerLoggedIn;
 	// should be verify (ID == m2024) (Passward == 00)
-	public void Login(string ID, string Password)
-	{ 
-		ID = "m2024";
-		Password = "00";
+	public string Login(string id, string password)
+	{
+		id = "m2024";
+		password = "00";
 		// Method for manager login
-		if (this.ID == ID && this.Password == Password)
+		if (this.ID == id && this.Password == password)
 		{
-			Console.WriteLine("Log-in is successfully done!");
+			return ("Log-in is successfully done!");
+			IsManagerLoggedIn = true;
 		}
 		else
 		{
-			Console.WriteLine("Error, re-check your Password or ID"); ;
-	    }
+			return ("Error, re-check your Password or ID");
+			IsManagerLoggedIn =false;
+		}
 	}
 	public void Logout()
 	{
+		if (IsManagerLoggedIn)
+		{
+
+		}
 		// Method for manager logout
-	}
-	public void NewUser(string ID, string Name, string Password) { } // adding new manager to the system who does not have account 
+	} 
 }
 //=========================================================//
 [Serializable]
 public class Room
 {
 	private int number;
-	public int Number 
+	public int Number
 	{
 		get
 		{
@@ -148,7 +149,7 @@ public class Room
 		}
 	} // Room number
 	private string type;
-	public string Type 
+	public string Type
 	{
 		get
 		{
@@ -160,10 +161,10 @@ public class Room
 		}
 	} // Room type
 	private double pricePerDay;
-	public double PricePerDay 
+	public double PricePerDay
 	{
-		get 
-		{ 
+		get
+		{
 			return pricePerDay;
 		}
 		set
@@ -183,7 +184,7 @@ public class Room
 
 		}
 	} // Room availability
-	public Room(int Number, string Type, double PricePerDay, bool AvailabilityStatus) 
+	public Room(int Number, string Type, double PricePerDay, bool AvailabilityStatus)
 	{
 		this.number = Number;
 		this.type = Type;
@@ -213,19 +214,12 @@ public class Reservation
 
 		}
 	}
-	private int roomnumber;
-	public int RoomNumber 
-	{
-		get
-		{
-			return this.roomnumber;
-		}
-		set
-		{
 
-		}
-	}
-	//**********************************************************************
+	//********************************************************************** 
+	private int roomnumber;
+	public int RoomNumber
+	{
+	} // we can take it from class room get 
 	private string GuestID { get; set; } // Guest ID (from Guest class) we may get iy from guest class when needed  
 	//**********************************************************************
 	private string CheckIndate;
@@ -312,10 +306,10 @@ public class Payment
 
 		}
 	} // bill number 
-//**********************************************************************
+	  //**********************************************************************
 	private string ReservationID { get; set; } // could be deleted and get fron reservation class when needed 
-	// as well as bank balance 
-//**********************************************************************
+											   // as well as bank balance 
+	//**********************************************************************
 	private double Totalamount;
 	public double TotalAmount
 	{
@@ -352,7 +346,7 @@ public class Payment
 
 		}
 	}  // Payment status ("paid", "not paid")
-	public bool IsbalancePayable(double TotalAmount, Guest g) 
+	public bool IsbalancePayable(double TotalAmount, Guest g)
 	{
 		double Balance = g.BankBalance;
 		return TotalAmount <= Balance;
@@ -380,15 +374,20 @@ public class IOFiles
 	public void GuestFile()
 	{
 		FileStream GuestsData = new FileStream("GuestsFile.txt", FileMode.Create, FileAccess.Write);
-		Guest g0 = new Guest("12345","Omar","11",05215,450);
-		Guest g1 = new Guest("12546","Khaled", "22",01459,550);
-		Guest g2 = new Guest("16556","Salma", "33",04122,660);
-		Guest g3 = new Guest("18730","Ahmad", "44",02250,720);
+		Guest g0 = new Guest("12345", "Omar", "11", 05215, 450);
+		Guest g1 = new Guest("12546", "Khaled", "22", 01459, 550);
+		Guest g2 = new Guest("16556", "Salma", "33", 04122, 660);
+		Guest g3 = new Guest("18730", "Ahmad", "44", 02250, 720);
+		//BinaryFormatter GDbf = new BinaryFormatter();
 		JsonSerializer.Serialize(GuestsData, g0);
 		JsonSerializer.Serialize(GuestsData, g1);
 		JsonSerializer.Serialize(GuestsData, g2);
 		JsonSerializer.Serialize(GuestsData, g3);
-		GuestsData.Close();
+		/*GDbf.Serialize(GuestsData, g0);
+		GDbf.Serialize(GuestsData, g1);
+		GDbf.Serialize(GuestsData, g2);
+		GDbf.Serialize(GuestsData, g3);
+		GuestsData.Close();*/
 	}
 	public void ReservationFile()
 	{
@@ -405,26 +404,25 @@ public class IOFiles
 	}
 	public void RoomsFile()
 	{
-
 		FileStream RoomsData = new FileStream("RoomsFile.txt", FileMode.Create, FileAccess.Write);
 		//int Number, string Type, double PricePerDay, bool AvailabilityStatus
-		Room r0 = new Room(442,"Single",25,true);
-		Room r1 = new Room(102,"Double",30,true);
-		Room r2 = new Room(506,"Double",32,false);
-		Room r3 = new Room(702,"Suit",40,true);
-		Room r4 = new Room(333,"Double",34,true);
+		Room r0 = new Room(442, "Single", 25, true);
+		Room r1 = new Room(102, "Double", 30, true);
+		Room r2 = new Room(506, "Double", 32, false);
+		Room r3 = new Room(702, "Suit", 40, true);
+		Room r4 = new Room(333, "Double", 34, true);
+		//BinaryFormatter RDbf = new BinaryFormatter();
+		/*RDbf.Serialize(RoomsData, r0);
+		RDbf.Serialize(RoomsData, r1);
+		RDbf.Serialize(RoomsData, r2);
+		RDbf.Serialize(RoomsData, r3);
+		RDbf.Serialize(RoomsData, r4);*/
 		JsonSerializer.Serialize(RoomsData, r0);
 		JsonSerializer.Serialize(RoomsData, r1);
 		JsonSerializer.Serialize(RoomsData, r2);
 		JsonSerializer.Serialize(RoomsData, r3);
 		JsonSerializer.Serialize(RoomsData, r4);
 		RoomsData.Close();
-
-
-
-
-
-
 		/*using (FileStream RoomsData = new FileStream("RoomsFile.txt", FileMode.Create, FileAccess.Write))
 		using (StreamWriter writer = new StreamWriter(RoomsData))
 		{
@@ -440,12 +438,11 @@ public class IOFiles
 			{
 				string json = JsonSerializer.Serialize(room); writer.WriteLine(json);
 			}*/
-
-
 	}
+}
 //=========================================================//
 [Serializable]
-public class Service 
+public class Service
 {
 	private string id;
 	public string ID
@@ -471,9 +468,9 @@ public class Service
 
 		}
 	}  // Service notes
-//**********************************************************************
+	   //**********************************************************************
 	private string GuestID { get; set; } // Guest ID (from Guest class)
-//**********************************************************************
+										 //**********************************************************************
 
 	int i = 0;
 	private string[] description = { "Car-Rental", "Kids-Zone" }; // Service types
@@ -494,148 +491,269 @@ public class Service
 		// Method to calculate the service amount
 	}
 }
-	//=========================================================//
-	public class HotelSystem : User
+//=========================================================//
+public class HotelSystem 
+{
+	private List<Guest> Guests;
+	private List<Room> Rooms;
+	private List<Reservation> Reservations;
+	private List<Payment> Payments;
+	private List<Service> Services;
+	private IOFiles ioFiles;
+	public HotelSystem()
 	{
-		private List<Guest> Guests;
-		private List<Room> Rooms;
-		private List<Reservation> Reservations;
-		private List<Payment> Payments;
-		private List<Service> Services;
-		private IOFiles ioFiles;
-		public HotelSystem()
+		ioFiles = new IOFiles();
+		ioFiles.GuestFile();
+		ioFiles.RoomsFile();
+		ioFiles.ReservationFile();
+		ioFiles.PaymentFile();
+		ioFiles.ServiceFile();
+		Rooms = LoadRoomsFromFile();
+		Guests = LoadGuestsFromFile();
+		Payments = LoadPaymentsFromFile();
+		Services = LoadServicesFromFile();
+		Reservations = LoadReservationsFromFile();
+	}
+	public string LoginG()
+	{
+		List<Guest> Guests = LoadGuestsFromFile();
+		Console.Write("Enter your ID : ");
+		string id = Console.ReadLine();
+		while (id.Length != 5)
 		{
-			ioFiles = new IOFiles();
-			ioFiles.GuestFile();
-			ioFiles.RoomsFile();
-			ioFiles.ReservationFile();
-			ioFiles.PaymentFile();
-			ioFiles.ServiceFile();
-			Rooms = LoadRoomsFromFile();
-			Guests = LoadGuestsFromFile();
-			Payments = LoadPaymentsFromFile();
-			Services = LoadServicesFromFile();
-			Reservations = LoadReservationsFromFile();
+			Console.Write("Pleas enter valid ID (Note: Of 5 digits) : ");
+			id = Console.ReadLine();
 		}
-		public void Login(string ID, string Password) { }
-		public void Logout() { }
-		public void NewUser(string ID, string Name, string Password) { }
-		public void AddGuest(Guest guest)
+		Console.WriteLine("");
+		Console.Write("Enter Your Password : ");
+		string password = Console.ReadLine();
+		while (password.Length != 2)
 		{
-			Guests.Add(guest);
-			SaveGuestToFile(guest);
+			Console.Write("Pleas enter correct Password : ");
+			password = Console.ReadLine();
 		}
-		public void AddRoom(Room room)
+		bool p = Guests.Contains(new Guest(id,password));
+		bool IsGuestLoggedIn = true;
+		if (p)
 		{
-			Rooms.Add(room);
-			SaveRoomToFile(room);
+			return ("Log-in process is successfully done! Welcome");
+			IsGuestLoggedIn = true;
 		}
-		public void AddReservation(Reservation reservation)
+		else
 		{
-			Reservations.Add(reservation);
-			SaveReservationToFile(reservation);
+			return ("Error, re-check your Password or ID, it does not compatable with any that in our system!");
+			IsGuestLoggedIn = false;
 		}
-		public void AddPayment(Payment payment)
+
+	}
+	public void LogoutG()
+	{
+		string b = LoginG();
+		if (b == "Log-in process is successfully done! Welcome")
 		{
-			Payments.Add(payment);
-			SavePaymentToFile(payment);
+			Console.WriteLine("Thanks for Choosing us! We hope you had a delightful experience and look forward to welcoming you back soon.\nGoodbye!");
 		}
-		public void AddService(Service service)
+		
+	}
+	public void NewUser(/*string ID, string Name, string Password, double PhoneNumber, double Bankbalance*/) 
+	{
+	//	string ID, string Name, string Password, double PhoneNumber, double Bankbalance
+		Console.Write("Enter your Name : ");
+		string name = Console.ReadLine();
+		while (name == " ")
 		{
-			Services.Add(service);
-			SaveServiceToFile(service);
+				Console.Write("Pleas enter valid Name (Note: Readable Name) : ");
+				name = Console.ReadLine();
 		}
-		public void ViewAllGuests()
+		Console.WriteLine("");
+		Console.Write("Enter your ID : ");
+		string id = Console.ReadLine();
+		while (id.Length != 5)
 		{
-			// Code to display all guests
+				Console.Write("Pleas enter valid ID (Note: Of 5 digits) : ");
+				id = Console.ReadLine();
 		}
-		public void ViewAllRooms()
+		Console.WriteLine("");
+		Console.Write("Make a Password : ");
+		string password = Console.ReadLine();
+		while (password.Length != 2)
 		{
-			// Code to display all rooms
+				Console.Write("Pleas enter valid Password (Note: Of 2 digits) : ");
+				password = Console.ReadLine();
 		}
-		public void ViewAllReservations()
+		Console.WriteLine("");
+		Console.Write("Enter your PhoneNumber : ");
+		string PN = Console.ReadLine();
+		double phonenumber = Convert.ToDouble(PN);
+		while (PN.Length != 5 || phonenumber == 00000)
 		{
-			// Code to display all reservations
+			Console.Write("Please enter a valid Phone Number (Note: Of 5 digits) : ");
+			PN = Console.ReadLine();
+			phonenumber = Convert.ToDouble(PN);
 		}
-		public void ViewAllPayments()
+		Console.WriteLine("");
+		Console.Write("Enter your Bank information (Balance) : ");
+		double Balance = Convert.ToDouble(Console.ReadLine());
+		while (Balance == 0)
 		{
-			// Code to display all payments
+			Console.Write("Pleas enter valid Bank Account : ");
+			Balance = Convert.ToDouble(Console.ReadLine());
 		}
-		public void ViewAllServices()
+		Console.WriteLine("");
+		Guest NewGuest = new Guest(name,id,password,phonenumber,Balance);
+		Console.WriteLine("Your registration is successfully done! Welcome ");
+		SaveGuestToFile(NewGuest);
+	}
+	public void SaveGuestToFile(Guest guest)
+	{
+		FileStream GuestsData = new FileStream("GuestsFile.txt", FileMode.Open, FileAccess.Write);
+		JsonSerializer.Serialize(GuestsData, guest);
+		GuestsData.Close();
+	}
+	public List<Guest> LoadGuestsFromFile()
+	{
+		using (FileStream GuestsData = new FileStream("GuestsFile.txt", FileMode.Open, FileAccess.Read))
+		using (StreamReader reader = new StreamReader(GuestsData)) 
 		{
-			// Code to display all services
-		}
-		// Methods moved from IOFiles
-		public void SaveGuestToFile(Guest guest)
-		{
-			// Code to save guest data to file
-		}
-		public List<Guest> LoadGuestsFromFile()
-		{
-			// Code to load guest data from file
-			return new List<Guest>();
-		}
-		public void SaveReservationToFile(Reservation reservation)
-		{
-			// Code to save reservation data to file
-		}
-		public List<Reservation> LoadReservationsFromFile()
-		{
-			// Code to load reservation data from file
-			return new List<Reservation>();
-		}
-		public void SaveServiceToFile(Service service)
-		{
-			// Code to save service data to file
-		}
-		public List<Service> LoadServicesFromFile()
-		{
-			// Code to load service data from file
-			return new List<Service>();
-		}
-		public void SavePaymentToFile(Payment payment)
-		{
-			// Code to save payment data to file
-		}
-		public List<Payment> LoadPaymentsFromFile()
-		{
-			// Code to load payment data from file
-			return new List<Payment>();
-		}
-		public void SaveRoomToFile(Room room)
-		{
-			// Code to save room data to file
-		}
-		public List<Room> LoadRoomsFromFile()
-		{
-			// Code to load room data from file
-			return new List<Room>();
+			string jsonContent = reader.ReadToEnd();
+			return JsonSerializer.Deserialize<List<Guest>>(jsonContent);
 		}
 	}
+	public void AddRoom(Room room)
+	{
+		Rooms.Add(room);
+		SaveRoomToFile(room);
+	}
+	public void SaveRoomToFile(Room room)
+	{
+		// Code to save room data to file
+	}
+	public void AddReservation(Reservation reservation)
+	{
+		Reservations.Add(reservation);
+		SaveReservationToFile(reservation);
+	}
+	public void SaveReservationToFile(Reservation reservation)
+	{
+		// Code to save reservation data to file
+	}
+	public void AddPayment(Payment payment)
+	{
+		Payments.Add(payment);
+		SavePaymentToFile(payment);
+	}
+	public void AddService(Service service)
+	{
+		Services.Add(service);
+		SaveServiceToFile(service);
+	}
+	public void ViewAllGuests()
+	{
+		// Code to display all guests
+	}
+	public void ViewAllRooms()
+	{
+		// Code to display all rooms
+	}
+	public void ViewAllReservations()
+	{
+		// Code to display all reservations
+	}
+	public void ViewAllPayments()
+	{
+		// Code to display all payments
+	}
+	public void ViewAllServices()
+	{
+		// Code to display all services
+	}
+	// Methods moved from IOFiles
+	public List<Reservation> LoadReservationsFromFile()
+	{
+		// Code to load reservation data from file
+		return new List<Reservation>();
+	}
+	public void SaveServiceToFile(Service service)
+	{
+		// Code to save service data to file
+	}
+	public List<Service> LoadServicesFromFile()
+	{
+		// Code to load service data from file
+		return new List<Service>();
+	}
+	public void SavePaymentToFile(Payment payment)
+	{
+		// Code to save payment data to file
+	}
+	public List<Payment> LoadPaymentsFromFile()
+	{
+		// Code to load payment data from file
+		return new List<Payment>();
+	}
+	public List<Room> LoadRoomsFromFile()
+	{
+		// Code to load room data from file
+		return new List<Room>();
+	}
+}
 namespace Program
 {
 	class program
 	{
-		static void Main()
+		static void Main(string[] args)
 		{
 			HotelSystem hotelSystem = new HotelSystem();
-			Console.WriteLine("Welcome to our hotel self-service system, You're here as ?\nPleas select a number \n\n\n1.Guest \n2.Manager");
-
+			Console.WriteLine("Welcome to our hotel self-service system, You're here as ?\nPleas select a number \n\n\n1.Log-in as Guest \n2.Log-in as Manager \n3.Sign-in as new Guest \n4.Exit");
 			int KindofUser = Convert.ToInt32(Console.ReadLine());
-			switch (KindofUser) 
+			/*if (args.Length > 0 && args[0] == "NewUser")
 			{
-
+				hotelSystem.NewUser();
+			}*/
+			switch (KindofUser)
+			{
 				case 1:
-					Console.WriteLine("Guest");
+					hotelSystem.LoginG();
 					break;
 				case 2:
 					Console.WriteLine("Manager");
 					break;
+				case 4:
+					hotelSystem.Logout();
+					break;
+				case 3:
+                    /*ProcessStartInfo psi = new ProcessStartInfo
+					{
+						FileName = "cmd.exe",
+						Arguments = $"/c start \"dotnet run -- NewUser\"",
+						UseShellExecute = true
+					};
+					Process.Start(psi);*/
+					hotelSystem.NewUser();
+					break;
 				default:
-					Console.WriteLine("Try Again");
-				break;
+					Console.Write("Try Again");
+					break;
 			}
 
+			//******************************************************
+
+			/*if (KindofUser == "1")
+			{
+				Console.WriteLine("Guest");
+			}
+			else if (KindofUser == "2")
+			{
+				Console.WriteLine("Manager");
+			}
+			else if (KindofUser == "3")
+			{
+				hotelSystem.Logout();
+			} 
+			else
+			{
+				Console.WriteLine("Try Again");
+			}*/
 		}
 	}
 }
